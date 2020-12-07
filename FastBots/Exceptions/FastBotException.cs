d08fd29c0ed3046fda434b4d48def5aead58211c
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-12-07 22:29:37 +0200

    Init commit

---
+++ FastBots/Exceptions/FastBotException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastBots.Exceptions
+{
+    public class FastBotException : Exception
+    {
+
+    }
+}
---
+++ FastBots/Extensions/IServicesCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastBots.Extensions
+{
+    public static class IServicesCollectionExtension
+    {
+        public static IServiceCollection AddFastBotsLibrary(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
