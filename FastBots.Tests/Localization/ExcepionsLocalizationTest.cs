bd705d7aa794f0e7518ea2108745655690ae56e1
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-12-12 02:31:10 +0200

    Example in tests

---
+++ FastBots.Tests/Localization/ExcepionsLocalizationTest.cs
@@ -0,0 +1,22 @@
+﻿using FastBots.Tests.Localization.Templates;
+using System;
+using Xunit;
+
+namespace FastBots.Tests.Localization
+{
+    public class ExcepionsLocalizationTest
+    {
+        [Fact]
+        public void CheckLocalizationWithCaseAndParam()
+        {
+            try
+            {
+                throw new SomeException("ru", "pinkyhi");
+            }
+            catch(Exception ex)
+            {
+                Assert.Equal(actual: ex.Message, expected: "Текст первого случая с параметром pinkyhi");
+            }
+        }
+    }
+}
---
+++ FastBots.Tests/Localization/Templates/SomeException.cs
@@ -0,0 +1,30 @@
+﻿using FastBots.Localization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastBots.Tests.Localization.Templates
+{
+    public class SomeException : Exception
+    {
+        static Dictionary<int, Dictionary<string, string>> LocalizationMap = new Dictionary<int, Dictionary<string, string>>
+        {
+            { 1, new Dictionary<string, string>
+                {
+                    { "en", "First case text with parameter {0}" },
+                    { "ru", "Текст первого случая с параметром {0}" }
+                }
+            },
+            { 2, new Dictionary<string, string>
+                {
+                    { "en", "Second case text with parameter {0}" },
+                    {  "ru", "Текст второго случая с параметром {0}" }
+                }
+            },
+        };
+
+        public SomeException(string languageCode, string username) : base(String.Format(Localizer.Localize(LocalizationMap, 1, languageCode), username))
+        {
+        }
+    }
+}
